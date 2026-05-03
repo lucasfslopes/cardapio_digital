147350fb454b13b28f46e1922162f1789081252e
lucasfslopes <lukasmdf2@gmail.com>
2026-05-02 22:09:15 -0300

    conectado itens com o JSON

--- src/js/script.js
+++ src/js/script.js
@@ -18,5 +18,27 @@ async function carregarItens() {
 }
 
 function mostrarItens() {
-    itens.forEach()
+    const content = document.getElementById('sectionCategoria1');
+
+    itens.forEach(p =>{
+        const cardItem = document.createElement('div');
+        cardItem.classList.add('cardItens');
+
+        cardItem.innerHTML = `
+            <div class="colunaInfoItem">
+                <h2>${p.nome_produto.toUpperCase()}</h2>
+                <p>${p.descricao}</p>
+                <div>${p.tag[0].toUpperCase()}</div>
+                <h3>R$ ${p.preco.toFixed(2)}</h3>
+            </div>
+
+            <div class="colunaFoto">
+                <img class="fotoItemCard" src="${p.link_foto}" alt="imagem ilustrativa">
+            </div>
+        `;
+
+        content.appendChild(cardItem);      
+    })
+
+    
 }
